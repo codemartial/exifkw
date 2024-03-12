62759d94daa5b8b767e5b5b9c26534b0caff1d2a
Tahir Hashmi <codemartial@gmail.com>
2024-03-12 10:10:35 +0700

    Allow ability to delete keywords using -- prefix

--- exifkw.go
+++ exifkw.go
@@ -9,22 +9,32 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		exit(1, fmt.Sprintln("Usage:", os.Args[0], "\"keyword[,keyword...\" paths"))
+		exit(1, fmt.Sprintln("Usage:", os.Args[0], "\"KEYWORD[,KEYWORD]...\" EXIFTOOL_OPTS... FILE..."))
 	}
 	args := []string{"-P", "-overwrite_original"}
 
 	for _, kw := range strings.Split(os.Args[1], ",") {
 		kw = strings.TrimSpace(kw)
+		delkw := false
+		if strings.HasPrefix(kw, "--") {
+			delkw = true
+			kw = strings.TrimPrefix(kw, "--")
+		}
 		if kw == "" {
 			continue
 		}
-		args = append(args, fmt.Sprintf("-keywords-=%s", kw), fmt.Sprintf("-keywords+=%s", kw))
-	}
+		args = append(args, fmt.Sprintf("-keywords-=%s", kw))
+		if !delkw {
+			args = append(args, fmt.Sprintf("-keywords+=%s", kw))
+		}
+	} // Build exiftool args for adding keywords
+
+	args = append(args, os.Args[2:]...) // Append remaining exiftool args
+
 	if len(args) == 2 {
-		exit(2, "No keywords supplied")
+		exit(2, "No keywords or options supplied")
 	}
 
-	args = append(args, os.Args[2:]...)
 	cmd := exec.Command("exiftool", args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
